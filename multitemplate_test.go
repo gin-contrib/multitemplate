4b92988fb3c08400c0c84c9f97044e91b4fb39b0
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-12-21 10:09:33 +0800

    add files From Glob
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- multitemplate_test.go
+++ multitemplate_test.go
@@ -16,16 +16,23 @@ func performRequest(r http.Handler, method, path string) *httptest.ResponseRecor
 	return w
 }
 
-func createMyRender() Render {
+func createFromFile() Render {
 	r := New()
 	r.AddFromFiles("index", "tests/base.html", "tests/article.html")
 
 	return r
 }
 
+func createFromGlob() Render {
+	r := New()
+	r.AddFromGlob("index", "tests/global/*")
+
+	return r
+}
+
 func TestAddFromFiles(t *testing.T) {
 	router := gin.New()
-	router.HTMLRender = createMyRender()
+	router.HTMLRender = createFromFile()
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(200, "index", gin.H{
 			"title": "Test Multiple Template",
@@ -35,4 +42,18 @@ func TestAddFromFiles(t *testing.T) {
 	w := performRequest(router, "GET", "/")
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, "<p>Test Multiple Template</p>\nHi, this is article template\n", w.Body.String())
+}
+
+func TestAddFromGlob(t *testing.T) {
+	router := gin.New()
+	router.HTMLRender = createFromGlob()
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index", gin.H{
+			"title": "Test Multiple Template",
+		})
+	})
+
+	w := performRequest(router, "GET", "/")
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "<p>Test Multiple Template</p>\nHi, this is login template\n", w.Body.String())
 }
